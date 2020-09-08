ea29cdb30e56fc3d2346b04d3530a28a55990f17
Serban Mihai <ser13ban@gmail.com>
2020-09-08 14:23:51 +0300

    added README, fixed the sign bugg

---
+++ README.md
@@ -0,0 +1,11 @@
+# Just a simple calculator
+
+This is just a very simple calculator, you can add, subbstract, multiply and divide numbers.
+
+![](images/appPreview.png)
+
+## Tools that I've used:
+
+- html
+- css
+- JavaScript
--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -2,33 +2,33 @@ let currentResult = 0;
 function transformInputToNumber() {
   return parseInt(userInput.value);
 }
-function outPutWithSign(userNumber, result) {
+function outPutWithSign(userNumber, result, sign) {
   if (userNumber < 0) {
-    outputResult(result, `${currentResult} + (${userNumber})`);
-  } else outputResult(result, `${currentResult} + ${userNumber}`);
+    outputResult(result, `${currentResult} ${sign} (${userNumber})`);
+  } else outputResult(result, `${currentResult} ${sign} ${userNumber}`);
 }
 function add() {
   const userNumber = transformInputToNumber();
   const result = currentResult + userNumber;
-  outPutWithSign(userNumber, result);
+  outPutWithSign(userNumber, result, "+");
   currentResult = result;
 }
 function sub() {
   const userNumber = transformInputToNumber();
   const result = currentResult - userNumber;
-  outPutWithSign(userNumber, result);
+  outPutWithSign(userNumber, result, "-");
   currentResult = result;
 }
 function mul() {
   const userNumber = transformInputToNumber();
   const result = currentResult * userNumber;
-  outPutWithSign(userNumber, result);
+  outPutWithSign(userNumber, result, "*");
   currentResult = result;
 }
 function div() {
   const userNumber = transformInputToNumber();
   const result = currentResult / userNumber;
-  outPutWithSign(userNumber, result);
+  outPutWithSign(userNumber, result, "/");
   currentResult = result;
 }
 addBtn.addEventListener("click", add);
