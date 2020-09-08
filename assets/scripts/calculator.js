4859f67addb74a34202658b3c7af654271956b30
Serban Mihai <ser13ban@gmail.com>
2020-09-08 13:58:00 +0300

    added pharanthesis to the output function (when a number is negative)

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -1,36 +1,37 @@
 let currentResult = 0;
-function transformInputToNumber(){
-    return parseInt(userInput.value);
+function transformInputToNumber() {
+  return parseInt(userInput.value);
 }
-function add () {
-    const userNumber = transformInputToNumber();
-    const result = currentResult + userNumber;
-    outputResult(result,`${currentResult} + ${userNumber}`)
-    currentResult  = result;
-   
+function outPutWithSign(userNumber, result) {
+  if (userNumber < 0) {
+    outputResult(result, `${currentResult} + (${userNumber})`);
+  } else outputResult(result, `${currentResult} + ${userNumber}`);
 }
-function sub () {
-    const userNumber = transformInputToNumber();
-    const result = currentResult - userNumber;
-    outputResult(result,`${currentResult} - ${userNumber}`)
-    currentResult  = result;
-   
+function add() {
+  const userNumber = transformInputToNumber();
+  const result = currentResult + userNumber;
+  outPutWithSign(userNumber, result);
+  currentResult = result;
 }
-function mul () {
-    const userNumber = transformInputToNumber();
-    const result = currentResult * userNumber;
-    outputResult(result,`${currentResult} *  ${userNumber}`)
-    currentResult  = result;
-   
+function sub() {
+  const userNumber = transformInputToNumber();
+  const result = currentResult - userNumber;
+  outPutWithSign(userNumber, result);
+  currentResult = result;
 }
-function div () {
-    const userNumber = transformInputToNumber();
-    const result = currentResult / userNumber;
-    outputResult(result,`${currentResult} / ${userNumber}`)
-    currentResult  = result;
-   
+function mul() {
+  const userNumber = transformInputToNumber();
+  const result = currentResult * userNumber;
+  outPutWithSign(userNumber, result);
+  currentResult = result;
 }
-addBtn.addEventListener('click',add);
-subtractBtn.addEventListener('click',sub);
-multiplyBtn.addEventListener('click',mul);
-divideBtn.addEventListener('click', div);
+function div() {
+  const userNumber = transformInputToNumber();
+  const result = currentResult / userNumber;
+  outPutWithSign(userNumber, result);
+  currentResult = result;
+}
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", sub);
+multiplyBtn.addEventListener("click", mul);
+divideBtn.addEventListener("click", div);
